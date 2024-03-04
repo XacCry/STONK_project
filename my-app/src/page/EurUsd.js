dbbbfc914621017103070c8440a24fb8cbce1a83
XacCry <116536789+XacCry@users.noreply.github.com>
2024-03-04 23:24:40 +0700

    new

--- my-app/src/page/EurUsd.js
+++ my-app/src/page/EurUsd.js
@@ -9,8 +9,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import DownloadIcon from '@mui/icons-material/Download';
-import { FaDownload } from "react-icons/fa";
-import './css/EurUsd.css'
+import './css/EurUsd.css';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -26,53 +25,62 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
+
 const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+
 ];
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
+
 export const EurUsd = () => {
   return (
-    <div className='Container'>
-    <div style={{ width: '80%' }}>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 400 }} aria-label="customized table">
-                  <TableHead>
-                    <TableRow>
-                      <StyledTableCell>Number</StyledTableCell>
-                      <StyledTableCell >Timeframe</StyledTableCell>
-                      <StyledTableCell >Accurate</StyledTableCell>
-                      <StyledTableCell >model</StyledTableCell>
-                      <StyledTableCell align='center'>Rate </StyledTableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <StyledTableRow key={row.name}>
-                        <StyledTableCell component="th" scope="row">
-                          {row.name}
-                        </StyledTableCell>
-                        <StyledTableCell >{row.calories}</StyledTableCell>
-                        <StyledTableCell >{row.fat}</StyledTableCell>
-                        <StyledTableCell >{row.carbs}</StyledTableCell>
-                        <StyledTableCell align='center'> <Button variant="contained" color="success" ><DownloadIcon /></Button></StyledTableCell>
-                      </StyledTableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </div>
+    <div className='Container' style={{ height: '100vh', overflowY: 'auto' }}>
+      <div style={{ width: '80%' }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 400 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Number</StyledTableCell>
+                <StyledTableCell>Timeframe</StyledTableCell>
+                <StyledTableCell>Accurate</StyledTableCell>
+                <StyledTableCell>model</StyledTableCell>
+                <StyledTableCell align='center'>Rate </StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell>{row.calories}</StyledTableCell>
+                  <StyledTableCell>{row.fat}</StyledTableCell>
+                  <StyledTableCell>{row.carbs}</StyledTableCell>
+                  <StyledTableCell align='center'>
+                    <Button variant="contained" color="success">
+                      <DownloadIcon />
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 };
